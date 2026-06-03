17fe949a144ccaf7980eac5acd218b583fb8856d
Thamu <movieinsight908@gmail.com>
2026-06-03 11:47:39 +0530

    hide status bar natively in Android MainActivity

--- android/app/src/main/kotlin/com/thiraipedia/thiraipedia_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/thiraipedia/thiraipedia_app/MainActivity.kt
@@ -1,5 +1,19 @@
 package com.thiraipedia.thiraipedia_app
 
+import android.os.Bundle
+import android.view.View
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        window.decorView.systemUiVisibility = (
+            View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+            or View.SYSTEM_UI_FLAG_FULLSCREEN
+            or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+            or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+            or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+        )
+    }
+}
