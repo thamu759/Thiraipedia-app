81ffe59c9cf4f8ee225f12c5037ec81930785c47
Thamu <movieinsight908@gmail.com>
2026-06-03 10:00:58 +0530

    fix: splash screen not navigating after video completes

--- lib/screens/splash/splash_screen.dart
+++ lib/screens/splash/splash_screen.dart
@@ -11,30 +11,44 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   late VideoPlayerController _controller;
+  bool _navigated = false;
 
   @override
   void initState() {
     super.initState();
+
     _controller = VideoPlayerController.networkUrl(
       Uri.parse(
         'https://res.cloudinary.com/di0j4psxz/video/upload/v1780421526/Yellow_lines_form_abstract_shapes_202606022251_homgfq.mp4',
       ),
-    )..initialize().then((_) {
-        if (mounted) {
-          _controller.play();
-          setState(() {});
-        }
-      });
+    );
+
+    _controller.initialize().then((_) {
+      if (mounted) {
+        _controller.play();
+        setState(() {});
+      }
+    }).catchError((_) {
+      if (mounted) _goToHome();
+    });
+
     _controller.addListener(() {
       if (_controller.value.isCompleted && mounted) {
         _goToHome();
       }
     });
+
+    Future.delayed(const Duration(seconds: 10), () {
+      if (mounted) _goToHome();
+    });
   }
 
   void _goToHome() {
+    if (_navigated) return;
+    _navigated = true;
+    _controller.removeListener(() {});
+    _controller.pause();
     Navigator.pushReplacementNamed(context, '/');
-    _controller.dispose();
   }
 
   @override
