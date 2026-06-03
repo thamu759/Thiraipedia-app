febf03423f84417f10872e8387c95543472f5130
Thamu <movieinsight908@gmail.com>
2026-06-03 11:03:43 +0530

    fix: trailer player configuration error on mobile - open in YouTube app instead of WebView

--- lib/utils/html_utils_stub.dart
+++ lib/utils/html_utils_stub.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
-import 'package:webview_flutter/webview_flutter.dart';
 
 class FilePickerResult {
   final String? dataUrl;
@@ -26,59 +25,18 @@ void showTrailerPopup({
   required String containerId,
   required void Function(void Function()) setState,
 }) {
-  final controller = WebViewController()
-    ..setJavaScriptMode(JavaScriptMode.unrestricted)
-    ..loadRequest(Uri.parse(embedUrl));
   final uri = Uri.tryParse(embedUrl);
   String? videoId;
   if (uri != null && uri.pathSegments.length == 2) {
     videoId = uri.pathSegments[1];
   }
 
-  showDialog(
-    context: context,
-    barrierDismissible: false,
-    barrierColor: Colors.black,
-    builder: (ctx) => Scaffold(
-      backgroundColor: Colors.black,
-      body: SafeArea(
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.fromLTRB(4, 4, 4, 0),
-              child: Row(
-                children: [
-                  IconButton(
-                    icon: const Icon(Icons.close, color: Colors.white, size: 28),
-                    onPressed: () => Navigator.pop(ctx),
-                  ),
-                  if (videoId != null)
-                    TextButton.icon(
-                      onPressed: () => launchUrl(
-                        Uri.parse('https://youtu.be/$videoId'),
-                        mode: LaunchMode.externalApplication,
-                      ),
-                      icon: const Icon(Icons.open_in_new, color: Colors.white70, size: 18),
-                      label: const Text('Open in YouTube',
-                          style: TextStyle(color: Colors.white70, fontSize: 13, fontFamily: 'Poppins')),
-                    ),
-                ],
-              ),
-            ),
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 8),
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(12),
-                  child: WebViewWidget(controller: controller),
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
-    ),
-  );
+  if (videoId != null) {
+    launchUrl(
+      Uri.parse('https://youtu.be/$videoId'),
+      mode: LaunchMode.externalApplication,
+    );
+  }
 }
 
 void showTrailerOverlay({
